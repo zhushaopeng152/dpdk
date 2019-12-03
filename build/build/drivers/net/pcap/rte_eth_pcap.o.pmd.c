5561500f14bf58d236c95318ecab9267736c0c41
zhusp <linux_zhu@sina.com>
2019-12-03 15:28:08 +0800

    dpdk all file

---
+++ build/build/drivers/net/pcap/rte_eth_pcap.o.pmd.c
@@ -0,0 +1,1 @@
+const char net_pcap_pmd_info[] __attribute__((used)) = "PMD_INFO_STRING= {\"name\" : \"net_pcap\", \"params\" : \"rx_pcap=<string> tx_pcap=<string> rx_iface=<ifc> rx_iface_in=<ifc> tx_iface=<ifc> iface=<ifc> phy_mac=<int>infinite_rx=<0|1>\", \"pci_ids\" : []}";
---
+++ build/build/kernel/linux/kni/kni_misc.c
@@ -0,0 +1,1 @@
+/home/zhushaopeng/dpdk/dpdk-19.08/kernel/linux/kni/kni_misc.c
